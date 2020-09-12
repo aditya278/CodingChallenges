0620a8079a5a8e88d053ed28fcaba6951f3b6cda
Aditya Shukla <aditya.shukla278@gmail.com>
2020-09-12 15:38:03 +0530

    Added Regex check on the number

--- 24_DuckNumberFinder/checkDuckNumber.js
+++ 24_DuckNumberFinder/checkDuckNumber.js
@@ -1,7 +1,7 @@
 /*
 Check if a given number is a DUCK NUMBER or NOT!
 
-Note: A Duck number is a number which has atleast one non-leading 0 present in it.
+Note: A Duck number is a positive number which has atleast one non-leading 0 present in it.
 Ex: 12101021 - Is a Duck number
 00121 - Is NOT a Duck Number.
 */
@@ -9,6 +9,13 @@ Ex: 12101021 - Is a Duck number
 const readlinesync = require('readline-sync');
 
 function isDuckNumber(num) {
+
+    const regex = /^[-+]?[0-9]*$/;
+    if(!num.match(regex))
+        return false;
+
+    if(num[0] === '-')
+        return false;
 
     let len = num.length;
     var i = 0;
@@ -26,12 +33,20 @@ function isDuckNumber(num) {
 
 function main() {
 
+    console.clear();
     let number = readlinesync.question("Enter the Number: ");
     if(isDuckNumber(number)) {
         console.log(`${number} is a Duck Number!`);
     }
     else {
         console.log(`${number} is not a Duck Number!`);
+    }
+    console.log();
+    var choice = readlinesync.question("Want to check for another number?[Y/N]: ");
+    if(choice === 'Y' || choice === 'y')
+        main();
+    else {
+        console.log("Thanks! Bye!");
     }
 
 }
